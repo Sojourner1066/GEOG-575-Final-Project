07012ce900dacf0642dedcddf9c45f1ec1be6eb8
Sojourner1066 <morgangrundy@gmail.com>
2024-04-14 16:34:23 +0100

    wrote the code to create a dict of genre and ticketmaster ids

--- js/testing_ticketmast_api.js
+++ js/testing_ticketmast_api.js
@@ -1,16 +1,20 @@
 
 var apikey = 'VcXVvrZqh1bwyvCeGQQgoMomydmwFLtm',
+    //this key filters to only music events
     musicSegmentId = 'KZFzniwnSyZfZ7v7nJ',
-    size = 10,
+    size = 200,
     builtUrl = `https://app.ticketmaster.com/discovery/v2/events.json?classificationId=${musicSegmentId}&size=${size}&apikey=${apikey}`
+    // different urls to pull differnt data
+    // builtUrl = `https://app.ticketmaster.com/discovery/v2/classifications/genres.json?size=${size}&apikey=${apikey}`
+    // builtUrl = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=${apikey}`
+    // builtUrl = `https://app.ticketmaster.com/discovery/v2/classifications/genres/KnvZfZ7vA71.json?apikey=${apikey}`
+    // Original Example
+    //url:"https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=VcXVvrZqh1bwyvCeGQQgoMomydmwFLtm",
+    // filter by event music type
+    //url:"https://app.ticketmaster.com/discovery/v2/events.json?classificationId=KZFzniwnSyZfZ7v7nJ&size=10&apikey=VcXVvrZqh1bwyvCeGQQgoMomydmwFLtm",
 
 $.ajax({
     type:"GET",
-    // Original Example
-    //url:"https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=VcXVvrZqh1bwyvCeGQQgoMomydmwFLtm",
-    
-    // filter by event music type
-    //url:"https://app.ticketmaster.com/discovery/v2/events.json?classificationId=KZFzniwnSyZfZ7v7nJ&size=10&apikey=VcXVvrZqh1bwyvCeGQQgoMomydmwFLtm",
     url:builtUrl,
     async:true,
     dataType: "json",
@@ -18,6 +22,14 @@ $.ajax({
                 console.log(json);
                 // Parse the response.
                 // Do other things.
+                var genre_dict = {}
+                for (var i = 0; i < json._embedded.events.length; i++){
+                    var genre = json._embedded.events[i].classifications[0].genre
+                    if ((genre.name in genre_dict)==false){
+                        genre_dict[genre.name] = genre.id
+                    };
+                };
+                console.log(genre_dict)
              },
     error: function(xhr, status, err) {
                 // This time, we do not end up here!
