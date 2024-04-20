c978df33164110244d484a539509d79cabd7ee22
lindsaymcclintock <154388061+lindsaymcclintock@users.noreply.github.com>
2024-04-19 21:38:04 -0500

    Add map and directions
    Added Mapbox basemap and plugin for directions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 //insert code here!
+
+var map;
+mapboxgl.accessToken = 'pk.eyJ1IjoibG1jY2xpbnRvY2syIiwiYSI6ImNsbzY0c2IweTBnNXcycm84dnEyMXBvaHAifQ.gZQ4VJyURj991pygjGxm3w'
+
+
+//create the map
+map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/lmcclintock2/clv33n4v401xx01pebykv37ls', // style URL
+    center: [-100, 40], // starting position [lng, lat]
+    zoom: 4 // starting zoom
+
+});
+map.addControl(
+    new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    }),
+    'top-right'
+);
+
+
