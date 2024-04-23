ddeebc0ced608be223b4e4045a3276804e5942b5
Sojourner1066 <morgangrundy@gmail.com>
2024-04-23 14:59:36 +0100

    added code to return geojson from getRouteDistance function

--- js/distance_api.js
+++ js/distance_api.js
@@ -1,28 +1,77 @@
-// Replace 'YOUR_BING_MAPS_API_KEY' with your actual Bing Maps API key
-const apiKey = 'AnlY8QF0iexa84_PKbKa68Gevl_aafwuBNTi1CapzJCtXldlgqnJ7LOaZ30ux9XB';
+
+
 
 // Define the start and end coordinates (latitude, longitude)
-const startCoord = '47.6062,-122.3321'; // Example: Seattle, WA
-const endCoord = '34.0522,-118.2437'; // Example: Los Angeles, CA
+coordinates_list = ['47.6062,-122.3321','34.0522,-118.2437']
 
-// Construct the request URL
-const apiUrl = `https://dev.virtualearth.net/REST/v1/Routes/Driving?waypoint.1=${startCoord}&waypoint.2=${endCoord}&distanceUnit=mi&key=${apiKey}`;
+function getRouteDistance(coordinates_list){
+    geojson_to_return =  {
+        "type": "FeatureCollection",
+        "name": "line",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [
+            {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "MultiLineString",
+                    "coordinates": [
+                        [
+                        ]
+                    ]
+                }
+            }
+        ]
+    }
+    // const apiUrl = `https://dev.virtualearth.net/REST/v1/Routes/Driving?waypoint.1=${startCoord}&waypoint.2=${endCoord}&distanceUnit=mi&key=${apiKey}`;
+    var apiUrl = 'https://dev.virtualearth.net/REST/v1/Routes/Driving?o=json';
+    const apiKey = 'AnlY8QF0iexa84_PKbKa68Gevl_aafwuBNTi1CapzJCtXldlgqnJ7LOaZ30ux9XB';
+    
+    coordinates_list.forEach(coordSet =>{
+        apiUrl = apiUrl + `&waypoint.${coordinates_list.indexOf(coordSet)+1}=${coordSet}`
+        // if (coordinates_list.indexOf(coordSet)===0){
+        //     apiUrl = apiUrl + `&waypoint.1=${coordSet}`
+        // } else {
+        //     apiUrl = apiUrl + `&waypoint.${coordinates_list.indexOf(coordSet)+1}=${coordSet}`
+        // }
+    });
+    queryUrl = apiUrl + `&distanceUnit=mi&key=${apiKey}`
+    // console.log(apiUrl)
 
-// Make a GET request to the Bing Routes API
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    //console.log(data)
-    // Parse the response data
-     const route = data.resourceSets[0].resources[0];
-     const travelDistance = route.travelDistance;
-     const travelDuration = route.travelDuration;
+    // Make the API request
+    fetch(queryUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        // console.log(data);
+        
+        var distance = data.resourceSets[0].resources[0].travelDistance;
+        var duration = data.resourceSets[0].resources[0].travelDuration;
+        
+        geojson_to_return.features[0].properties['TravelDistanceMiles'] = distance;
+        geojson_to_return.features[0].properties['TravelDurationSeconds'] = duration;
+        
+        var route = data.resourceSets[0].resources[0].routeLegs[0].itineraryItems;
+        route.forEach(wpt =>{
+            var lat = wpt.maneuverPoint.coordinates[0];
+            var long = wpt.maneuverPoint.coordinates[1];
+            geojson_to_return.features[0].geometry.coordinates[0].push([long, lat]);
+        })
+        console.log(geojson_to_return);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+    //return geojson_to_return
+};
 
-    // Output route information
-    console.log('Route calculated successfully:');
-    console.log(`Distance: ${travelDistance} miles`);
-    console.log(`Duration: ${travelDuration} seconds`);
-  })
-  .catch(error => {
-    console.error('Error calculating route:', error);
-  });
+document.addEventListener('DOMContentLoaded',getRouteDistance(coordinates_list))
