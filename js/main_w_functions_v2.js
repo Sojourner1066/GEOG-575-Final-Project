6a7efaaa8287592f90d369c19b3498d9443053fa
Sojourner1066 <morgangrundy@gmail.com>
2024-04-27 18:19:34 +0100

    select drop downs working

--- js/main_w_functions_v2.js
+++ js/main_w_functions_v2.js
@@ -1,25 +1,32 @@
 // General info here
 
-genre_dict = {'Metal': 'KnvZfZ7vAvt',
-              'Other': 'KnvZfZ7vAvl',
-              'Pop': 'KnvZfZ7vAev',
-              'Rock': 'KnvZfZ7vAeA',
-              'Dance/Electronic': 'KnvZfZ7vAvF',
-              'Hip-Hop/Rap': 'KnvZfZ7vAv1',
-              'Latin': 'KnvZfZ7vAJ6',
-              'Alternative': 'KnvZfZ7vAvv',
-              'Country': 'KnvZfZ7vAv6',
-              'Jazz': 'KnvZfZ7vAvE',
-              'R&B': 'KnvZfZ7vAee',
+genre_dict = {'Alternative': 'KnvZfZ7vAvv',
               'Blues': 'KnvZfZ7vAvd',
               'Classical': 'KnvZfZ7v7nJ',
+              'Country': 'KnvZfZ7vAv6',
               'Dance': 'KnvZfZ7v7nI',
+              'Dance/Electronic': 'KnvZfZ7vAvF',
               'Folk': 'KnvZfZ7vAva',
-              'World': 'KnvZfZ7vAeF',
+              'Hip-Hop/Rap': 'KnvZfZ7vAv1',
+              'Jazz': 'KnvZfZ7vAvE',
+              'Latin': 'KnvZfZ7vAJ6',
+              'Metal': 'KnvZfZ7vAvt',
+              'Pop': 'KnvZfZ7vAev',
               'Reggae': 'KnvZfZ7vAed',
-
+              'Rock': 'KnvZfZ7vAeA',
+              'R&B': 'KnvZfZ7vAee',
+              'Other': 'KnvZfZ7vAvl',
+              'World': 'KnvZfZ7vAeF',
 }
 
+// Get a reference to the genre dropdown element
+// and populate it with options from the genre_dict
+const genreDropdown = document.getElementById('selectGenre');
+for (var key in genre_dict) {
+    const option = document.createElement('option');
+    option.textContent = key;
+    genreDropdown.appendChild(option);
+}
 
 var map = L.map('map', {
     center: [39,-95],
@@ -78,28 +85,33 @@ var popup = L.popup({
 
 function getConcertInfo(filter_keyword, filter_event_type, filter_event_date, genreID, map){
  
-//  this will remove any data from map so new data can be loaded
-  eventsLayer.clearLayers()
-
+//  this will remove any marker data from map so new data can be loaded
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+       layer.remove();
+    }
+  });
+// This function will impliment a date selection. If no nate was pass the function a default 
+// start and and date is assigned
   if (filter_event_date == null){
-    var eventStartDate = '2024-06-01T00:00:00Z';
-    var eventEndDate = '2024-08-31T23:59:00Z';
+    var eventStartDate = '2024-06-01T00:01:00Z';
+    var eventEndDate = '2024-08-31T23:59:59Z';
   } else {
-    var eventStartDate = `${filter_event_date}T00:00:00Z`;
-    var eventEndDate = `${filter_event_date}T23:59:00Z`;
+    var eventStartDate = `${filter_event_date}T00:01:00Z`;
+    var eventEndDate = `${filter_event_date}T23:59:59Z`;
   }
+
   
-  console.log(eventStartDate,eventEndDate)
   // Define Ticketmaster API endpoint and parameters
   var url = "https://app.ticketmaster.com/discovery/v2/events.json";
-  var apiKey = "VcXVvrZqh1bwyvCeGQQgoMomydmwFLtm"; // Replace with your Ticketmaster API key
+  var apiKey = "VcXVvrZqh1bwyvCeGQQgoMomydmwFLtm";
 
   // Parameters for the query this is how the data is filtered
-  // this will return only festivals in the summer months 
+  // this will return only events in the summer months 
   // in the us. The max number is set by size (200 max)
   var params = {
     apikey: apiKey,
-    classificationName: filter_event_type,
+    //classificationName: filter_event_type,
     keyword: filter_keyword,
     genreId: genreID,
     classificationId: 'KZFzniwnSyZfZ7v7nJ', // this is the filter to be only music events
@@ -112,6 +124,7 @@ function getConcertInfo(filter_keyword, filter_event_type, filter_event_date, ge
   // Construct query URL with parameters
   var queryString = new URLSearchParams(params).toString();
   var queryUrl = `${url}?${queryString}`;
+  console.log(queryUrl)
 
   var recordIcon = L.icon({
       iconUrl: 'img/record.png',
@@ -126,48 +139,62 @@ function getConcertInfo(filter_keyword, filter_event_type, filter_event_date, ge
       return response.json();
     })
     .then(data => {
-      // console.log(data);
-      var eventMarkerList = []
 
-      // Extract event information from the response
-      const events = data._embedded.events;
-      events.forEach(event => {
-        const eventName = event.name;
-        const eventDate = event.dates.start.localDate
-        // console.log(eventDate);
-        venues = event._embedded.venues;
-        venues.forEach(venue => {
-          const venueName = venue.name;
-          const latitude = venue.location.latitude;
-          const longitude = venue.location.longitude;
-          
-          // Popup with a button - still testing
-          // var popupContent = '<center><img src="' + event.images[1].url +'"class="popImg"/><br>' + '<text class="popTitle">' +
-          // eventName + '</text><br><text class="popInfo">' + venueName + '<br>' + eventDate + 
-          // '</text><br>' + '<a href ="' + event.url +'"><text class="popLink">Get Tickets</a></text><button id="tourBtn">Add Concert to Tour</button></center>'
+      if ('_embedded' in data){
+        // console.log(data);
+        var eventMarkerList = []
 
-          // Popup contents with no button
-          var popupContent = '<center><img src="' + event.images[1].url +'"class="popImg"/><br>' + '<text class="popTitle">' +
-          eventName + '</text><br><text class="popInfo">' + venueName + '<br>' + eventDate + 
-          '</text><br>' + '<a href ="' + event.url +'"><text class="popLink">Get Tickets</a></text></center>'
+        // Extract event information from the response
+        const events = data._embedded.events;
+        events.forEach(event => {
+          const eventName = event.name;
+          const eventDate = event.dates.start.localDate
+          // console.log(eventDate);
+          venues = event._embedded.venues;
+          venues.forEach(venue => {
+            const venueName = venue.name;
+            const latitude = venue.location.latitude;
+            const longitude = venue.location.longitude;
+            
+            // Popup with a button - still testing
+            // var popupContent = '<center><img src="' + event.images[1].url +'"class="popImg"/><br>' + '<text class="popTitle">' +
+            // eventName + '</text><br><text class="popInfo">' + venueName + '<br>' + eventDate + 
+            // '</text><br>' + '<a href ="' + event.url +'"><text class="popLink">Get Tickets</a></text><button id="tourBtn">Add Concert to Tour</button></center>'
 
-          // Add marker to map at coordinates of venue with a popup containing event info
-          marker = L.marker([latitude, longitude], {icon: recordIcon}).bindPopup(popupContent, {className: 'popStyle'});
-          eventMarkerList.push(marker);
-          //marker = L.marker([latitude, longitude], {icon: recordIcon}).addTo(map).bindPopup(popupContent, {className: 'popStyle'});    
+            // Popup contents with no button
+            var popupContent = '<center><img src="' + event.images[1].url +'"class="popImg"/><br>' + '<text class="popTitle">' +
+            eventName + '</text><br><text class="popInfo">' + venueName + '<br>' + eventDate + 
+            '</text><br>' + '<a href ="' + event.url +'"><text class="popLink">Get Tickets</a></text></center>'
+
+            // Add marker to map at coordinates of venue with a popup containing event info
+            marker = L.marker([latitude, longitude], {icon: recordIcon}).bindPopup(popupContent, {className: 'popStyle'});
+            eventMarkerList.push(marker);   
+          });
         });
-      });
-      L.layerGroup(eventMarkerList).addLayer(eventsLayer).addTo(map);
+        L.layerGroup(eventMarkerList).addLayer(eventsLayer).addTo(map);
+      };
     })
     .catch(error => {
       console.error("Error:", error);
     });
 };
 
-document.addEventListener('DOMContentLoaded',getConcertInfo(filter_keyword='', filter_event_type='Concert', filter_event_date=null, genreID=genre_dict['Rock'], map))
+document.addEventListener('DOMContentLoaded',getConcertInfo(filter_keyword='', filter_event_type=null, filter_event_date=null, genreID=genre_dict['Rock'], map))
 
 const dateInput = document.getElementById("festivalDate");
 dateInput.addEventListener('change', function(selectedDate) {
-    console.log('Selected date:', selectedDate.target.value);
-    getConcertInfo(filter_keyword='', filter_event_type='Concert', filter_event_date=selectedDate.target.value, genreID=genre_dict['Rock'], map);
+    getConcertInfo(filter_keyword='', filter_event_type='', filter_event_date=selectedDate.target.value, genreID=genre_dict['Rock'], map);
+});
+
+// Add an event listener for the 'change' event
+genreDropdown.addEventListener('change', function(selectedGenre) {
+  console.log(selectedGenre.target.value)
+  getConcertInfo(filter_keyword='', filter_event_type='', filter_event_date=null, genreID=genre_dict[selectedGenre.target.value], map);  
+});
+
+const eventTypeDropdown = document.getElementById('selectEventType');
+// Add an event listener for the 'change' event
+eventTypeDropdown.addEventListener('change', function(selectedEventType) {
+  console.log(selectedEventType.target.value)
+  getConcertInfo(filter_keyword='', filter_event_type=selectedEventType.target.value, filter_event_date=null, genreID=genre_dict['Rock'], map);  
 });
